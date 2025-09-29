9239ec10d194288873417372b6c1d47cce755da8
Chamssoudine <chsak@gmail.com>
2025-09-29 23:53:16 +0200

    Pensez à rajouter dans importSupabase votre clé et URL

--- importSupabase.js
+++ importSupabase.js
@@ -3,7 +3,7 @@
  */
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJwdXZwdmRmeXpjdmRyZWRuYm5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg4NzI4NTIsImV4cCI6MjA3NDQ0ODg1Mn0.9g41exSxhUXMbuNLZkPO50oAofj-Dwjyvokrv-JF_bU'
-const supabaseUrl = 'https://bpuvpvdfyzcvdrednbnm.supabase.co'
+const SUPABASE_KEY = 'Mettez votre clé'
+const supabaseUrl = 'votre_url_Supabase'
 const supabaseKey = SUPABASE_KEY;
 export const supabase = createClient(supabaseUrl, supabaseKey)
